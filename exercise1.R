9bd9fac52f6405060ccaaad277af3b3baf170409
Melody Yu <yu.musicssis@gmail.com>
2017-04-04 14:17:55 -0700

    exercise1

---
+++ exercise1.R
@@ -0,0 +1,10 @@
+# Comment
+hometown <- Hsinchu
+my.name <- Melody Yu
+my.height <- 166
+puppies <- 2
+puppy.price <- 100
+total.cost <- puppies * puppy.price
+too.expensive <- total.cost > 1000
+max.puppies <- 1000/puppy.price
+
